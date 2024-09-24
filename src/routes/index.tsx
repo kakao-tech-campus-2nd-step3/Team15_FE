daba80e65016addb1fab3a6ebabf4ec4e6bfccf1
joshuayeyo <chiikawayeyo@gmail.com>
2024-09-24 13:49:40 +0900

    Fix: 페이지 구현 전까지 주석처리

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,34 +1,34 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "@/pages/Home";
-import LoginPage from "@/pages/Login";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import HomePage from "@/pages/Home";
+// import LoginPage from "@/pages/Login";
 
-import { RouterPath } from "./path";
+// import { RouterPath } from "./path";
 
-const router = createBrowserRouter([
-    {
-        path: RouterPath.home,
-        element: <HomePage />,
-    },
-    // {
-    //     path: RouterPath.layout,
-    //     element: <Layout />,
-    //     children: [
-    //         {
-    //             path: RouterPath.myPage,
-    //             element: <MyPage />
-    //         },
-    //         {
-    //             path: RouterPath.challenge,
-    //             element: <ChallengeFeedPage />
-    //         }
-    //     ]
-    // },
-    {
-        path: RouterPath.login,
-        element: <LoginPage />,
-    },
-]);
+// const router = createBrowserRouter([
+//     {
+//         path: RouterPath.home,
+//         element: <HomePage />,
+//     },
+//     // {
+//     //     path: RouterPath.layout,
+//     //     element: <Layout />,
+//     //     children: [
+//     //         {
+//     //             path: RouterPath.myPage,
+//     //             element: <MyPage />
+//     //         },
+//     //         {
+//     //             path: RouterPath.challenge,
+//     //             element: <ChallengeFeedPage />
+//     //         }
+//     //     ]
+//     // },
+//     {
+//         path: RouterPath.login,
+//         element: <LoginPage />,
+//     },
+// ]);
 
-export const Routes = () => {
-    return<RouterProvider router={router} />
-}
+// export const Routes = () => {
+//     return<RouterProvider router={router} />
+// }
