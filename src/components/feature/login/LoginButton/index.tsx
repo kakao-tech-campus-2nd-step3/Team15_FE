26803ce6786b2a2b58ba01999a09642743c6068c
eunjin <eunjin0321@naver.com>
2024-09-28 23:36:43 +0900

    Feat1: LoginButton 구현

---
+++ src/components/feature/login/LoginButton/index.tsx
@@ -0,0 +1,30 @@
+import { Button } from '@chakra-ui/react';
+
+const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
+const REDIRECT_URI = 'http://localhost:3000/';  
+
+const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+const kakaoButtonStyles = {
+  backgroundColor: "#ffeb00",   
+  color: "#000000",            
+  _hover: { backgroundColor: "#e6d700" }, 
+  borderRadius: "md",           
+  padding: "8px 16px",          
+  fontWeight: "bold",          
+  fontSize: "16px",          
+};
+
+const LoginButton = () => {
+  const handleLogin = () => {
+    window.location.href = KAKAO_AUTH_URL;
+  };
+
+  return (
+    <Button onClick={handleLogin} {...kakaoButtonStyles}>
+      카카오 로그인
+    </Button>
+  );
+};
+
+export default LoginButton;
