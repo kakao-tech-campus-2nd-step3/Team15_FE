8435f21fd56629dbc2c76f195a66730efe37b54c
joshuayeyo <chiikawayeyo@gmail.com>
2024-10-02 18:26:08 +0900

    Style(34): Card common component CSS 변경

--- src/components/common/Card/index.tsx
+++ src/components/common/Card/index.tsx
@@ -1,6 +1,6 @@
 import { Heading, Card, CardBody, Avatar, Flex, Text } from '@chakra-ui/react';
 import CommonImage from '@/components/common/Image';
-import styled from '@emotion/styled';
+
 
 type Props = {
     maxWidth: string;
@@ -19,17 +19,16 @@ const CommonCard = ({ maxWidth, imageURL, title, content, profileImage ,username
                     <CommonImage 
                         src={imageURL}
                         ratio={"square"}
-                        width={"500px"}
                     />
                     <br/>
                     <Heading size='lg'>{title}</Heading>
                     <Text size="sm">{content}</Text>
-                    <Flex gap='4'alignItems='center' justifyContent='flex-end' flexWrap='wrap'>
+                    <Flex gap='2'alignItems='center' justifyContent='flex-end' flexWrap='wrap'>
                         <Avatar size="sm" src={profileImage}/>
                         <Heading size="sm">{username}</Heading>
                     </Flex>
                 </CardBody>
-                </Card>
+            </Card>
         </>
 
     )
