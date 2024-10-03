9d55ab6456f2e4e3cf0d977e94bd2070b0f256ab
eunjin <eunjin0321@naver.com>
2024-10-03 21:08:36 +0900

    Feat: 백 인증코드 전달  주석처리

--- src/pages/Oauth/index.tsx
+++ src/pages/Oauth/index.tsx
@@ -1,6 +1,24 @@
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+// import axios from 'axios';
+
 const OauthPage = () => {
-  const code = new URL(window.location.href).searchParams.get("code");
-  console.log(code);
+  const location = useLocation();
+  
+  useEffect(() => {
+    // const code = new URLSearchParams(location.search).get('code');
+    // if (code) {
+    //   axios.post('', { code })
+    //     .then(response => { 
+    //       localStorage.setItem('access_token', response.data.accessToken);
+    //       localStorage.setItem('refresh_token', response.data.refreshToken);
+    //       window.location.href = '/home';
+    //     })
+    //     .catch(error => {
+    //       console.error('로그인 실패:', error);
+    //     });
+    // }
+  }, [location]);
 }
 
 export default OauthPage;
