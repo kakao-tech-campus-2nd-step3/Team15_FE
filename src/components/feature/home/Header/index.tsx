4aa3a13e005c7b5ab1245ed9626e7b985004ff63
joshuayeyo <chiikawayeyo@gmail.com>
2024-10-04 14:01:28 +0900

    Style(20): 웰컴페이지 UI 변경

--- src/components/feature/home/Header/index.tsx
+++ src/components/feature/home/Header/index.tsx
@@ -15,7 +15,7 @@ const MainHeader = () => {
       <Left>
       </Left>
       <Right>
-        <GiHamburgerMenu size={30} color={'white'} onClick={toggleSide}/>
+        <GiHamburgerMenu size={'3vw'} color={'white'} onClick={toggleSide}/>
         <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
         <OverlayWrapper isOpen={isOpen}/>
       </Right>
