a93dcbeac06a1e9e0e7da7ee9ee7a0a60971b137
joshuayeyo <chiikawayeyo@gmail.com>
2024-10-10 11:44:46 +0900

    Fix(60): 데이터가 배열이 아닌 경우, 에러가 아닌 빈 페이지를 출력

--- src/components/feature/section/FeedItems/index.tsx
+++ src/components/feature/section/FeedItems/index.tsx
@@ -8,56 +8,61 @@ import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-
 // type Props = {
 //   id: number;
 //   imageUrl: string;
 //   title: string;
+//   type: string;
 // }
 
 const FeedItemSection = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [data, setData] = useState<any[]>([]);
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-
     async function getFeedData() {
       try {
         const res = await axios.get(`${BASE_URL}/api/feeds`);
-        // const res = await axios.get('/src/data/data.json');    // 아직 데이터가 없어서 에러가 뜹니다. mock 데이터 만들어서 테스트용입니다. POST 구현 후에는 삭제할 예정입니다.
         const result = await res.data;
         setData(result);
         setIsLoading(false);
       } catch (e) {
         console.log(e);
-        alert("Error: 데이터를 불러올 수 없습니다.");
+        alert('Error: 데이터를 불러올 수 없습니다.');
       }
-  }
-  getFeedData();
-}, []);
+    }
+    getFeedData();
+  }, []);
 
-  console.log(data)
+  console.log(data);
 
   const handleModalClose = () => {
     setIsModalOpen(false);
   };
 
+  if (!Array.isArray(data)) {
+    console.log('데이터가 배열이 아님!!');
+  }
+
   if (!data) return <></>;
-  if (isLoading) return <><Spinner /></>;
+  if (isLoading)
+    return (
+      <>
+        <Spinner />
+      </>
+    );
 
   return (
     <Wrapper>
       <CommonGrid columns={4} gap={50}>
-        {data.map((data) =>
+        {Array.isArray(data) &&
+          data.map((data) => (
             <ItemWrapper key={data.id} onClick={() => setIsModalOpen(true)}>
-              <FeedCard
-                imageUrl={data.imageUrl}
-                title={data.title}
-              />
+              <FeedCard imageUrl={data.imageUrl} title={data.title} />
             </ItemWrapper>
-        )}
-        </CommonGrid>
+          ))}
+      </CommonGrid>
       {isModalOpen && (
         <CommonModal
           isModalOpen={isModalOpen}
@@ -71,16 +76,16 @@ const FeedItemSection = () => {
 export default FeedItemSection;
 
 const Wrapper = styled.section`
-    margin-top: 2rem;
+  margin-top: 2rem;
   width: 100%;
-    height: auto;
-    display: flex;
-    flex-direction: row;
+  height: auto;
+  display: flex;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
 `;
 
 const ItemWrapper = styled.button`
-    width: 18vw;
-    min-width: 10vw;
+  width: 18vw;
+  min-width: 10vw;
 `;
