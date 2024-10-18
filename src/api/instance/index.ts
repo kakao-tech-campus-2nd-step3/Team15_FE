95570a97eaac64e323b917d36b887c2358cad548
eunjin <eunjin0321@naver.com>
2024-10-18 21:49:55 +0900

    feat: axios instance interceptors request 추가

--- src/api/instance/index.ts
+++ src/api/instance/index.ts
@@ -1,19 +1,33 @@
 import axios from 'axios';
 
 const baseURL = import.meta.env.VITE_BASE_URL;
-const jwtToken = localStorage.getItem('jwt_token');
-const parsedToken = jwtToken ? JSON.parse(jwtToken) : null;
-const accessToken = parsedToken?.accessToken;
-
 
 const instance = axios.create({
     baseURL: baseURL,
-    timeout: 10 * 10000,
+    timeout: 10 * 1000,
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
     },
 });
+
+instance.interceptors.request.use(
+    (config) => {
+        const jwtToken = localStorage.getItem('jwt_token');
+        const parsedToken = jwtToken ? JSON.parse(jwtToken) : null;
+        const accessToken = parsedToken?.accessToken;
+
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`;
+        } else {
+            delete config.headers.Authorization;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default instance;
